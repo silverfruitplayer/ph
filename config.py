797935a86e7cc108058e85dfd79d69df089649a1
Abhishek Sengupta <106603514+silverfruitplayer@users.noreply.github.com>
2023-12-19 20:32:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
 
     API_ID = 6
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-    BOT_TOKEN = "6560962385:AAFbWuz3MJRv83X_IRxu9jlB6q_Tt6gjlfI"
+    BOT_TOKEN = "6560962385:AAHnDjBDekBD3cE-fTIIKT2y8QkxV08uaIA"
